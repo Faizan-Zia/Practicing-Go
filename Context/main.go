b77050d9fac6ed714b11f7c48c32abe61cdb3ef0
Faizan-Zia <faizanzia005@gmail.com>
2022-08-13 20:39:17 +0500

    Added Context Cancellation
    Signed-off-by: Faizan-Zia <faizanzia005@gmail.com>

--- Context/main.go
+++ Context/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"context"
+	"time"
 )
 
 func doSomething(ctx context.Context) {
@@ -16,6 +17,38 @@ func doMore(ctx context.Context) {
 	fmt.Println("do more: ", ctx.Value("key"))
 }
 
+func assignTask(ctx context.Context) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+	churnNumbers := make(chan int)
+	go performTask(ctx, churnNumbers) 
+
+	for i := 0; i <= 3; i++ {
+		churnNumbers <- i
+	}
+
+	cancelCtx()
+
+	time.Sleep(1000 * time.Millisecond)
+
+	fmt.Println("Finished task assignment")
+}
+
+func performTask(ctx context.Context, ch chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Println("Cancel Sig Recvd: ", err)
+			}
+			fmt.Println("Finished Task")
+			return
+		case num := <-ch:
+			fmt.Println("Task Item:", num)
+		}
+
+	}
+}
+
 func main() {
 	ctx := context.Background()
 	
@@ -23,4 +56,7 @@ func main() {
 	ctx = context.WithValue(ctx, "key", "value")
 	
 	doSomething(ctx)
+
+	// Testing the cancel context functionality
+	assignTask(ctx)
 }
